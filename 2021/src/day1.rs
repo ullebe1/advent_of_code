12749cd173accc782ae9caf2be7588aeaeea8097
Ulrik Boll Djurtoft <ulrik.boll.djurtoft@lego.com>
2021-12-03 08:21:05 +0100

    Autoformat day1 and day2

--- 2021/src/day1.rs
+++ 2021/src/day1.rs
@@ -1,45 +1,45 @@
-use aoc_runner_derive::{aoc};
+use aoc_runner_derive::aoc;
 
 #[aoc(day1, part1)]
 pub fn part1(input: &str) -> usize {
     input
         .lines()
-        .map(|l| {
-            l.parse::<usize>().unwrap()
-        })
+        .map(|l| l.parse::<usize>().unwrap())
         .collect::<Vec<usize>>()
         .as_slice()
         .windows(2)
-        .fold(0, |sum, window| {
-            if window[0] < window[1] {
-                sum + 1
-            } else {
-                sum
-            } 
-        })
+        .fold(
+            0,
+            |sum, window| {
+                if window[0] < window[1] {
+                    sum + 1
+                } else {
+                    sum
+                }
+            },
+        )
 }
 
 #[aoc(day1, part2)]
 pub fn part2(input: &str) -> usize {
     input
         .lines()
-        .map(|l| {
-            l.parse::<usize>().unwrap()
-        })
+        .map(|l| l.parse::<usize>().unwrap())
         .collect::<Vec<usize>>()
         .as_slice()
         .windows(3)
-        .map::<usize, _>(|window| {
-            window.iter().sum()
-        })
+        .map::<usize, _>(|window| window.iter().sum())
         .collect::<Vec<usize>>()
         .as_slice()
         .windows(2)
-        .fold(0, |sum, window| {
-            if window[0] < window[1] {
-                sum + 1
-            } else {
-                sum
-            } 
-        })
+        .fold(
+            0,
+            |sum, window| {
+                if window[0] < window[1] {
+                    sum + 1
+                } else {
+                    sum
+                }
+            },
+        )
 }
--- 2021/src/day2.rs
+++ 2021/src/day2.rs
@@ -1,10 +1,10 @@
-use aoc_runner_derive::{aoc};
+use aoc_runner_derive::aoc;
 
 #[aoc(day2, part1)]
 pub fn part1(input: &str) -> usize {
     let mut depth = 0;
     let mut distance = 0;
-    
+
     let lines = input.lines();
     for line in lines {
         let mut parts = line.split(" ");
@@ -12,7 +12,7 @@ pub fn part1(input: &str) -> usize {
             "forward" => distance = distance + parts.next().unwrap().parse::<usize>().unwrap(),
             "down" => depth = depth + parts.next().unwrap().parse::<usize>().unwrap(),
             "up" => depth = depth - parts.next().unwrap().parse::<usize>().unwrap(),
-            _ => unreachable!()
+            _ => unreachable!(),
         }
     }
 
@@ -24,7 +24,7 @@ pub fn part2(input: &str) -> usize {
     let mut depth = 0;
     let mut distance = 0;
     let mut aim = 0;
-    
+
     let lines = input.lines();
     for line in lines {
         let mut parts = line.split(" ");
@@ -33,14 +33,10 @@ pub fn part2(input: &str) -> usize {
                 let x = parts.next().unwrap().parse::<usize>().unwrap();
                 distance = distance + x;
                 depth = depth + (aim * x);
-            },
-            "down" => {
-                aim = aim + parts.next().unwrap().parse::<usize>().unwrap()
-            },
-            "up" => {
-                aim = aim - parts.next().unwrap().parse::<usize>().unwrap()
-            },
-            _ => unreachable!()
+            }
+            "down" => aim = aim + parts.next().unwrap().parse::<usize>().unwrap(),
+            "up" => aim = aim - parts.next().unwrap().parse::<usize>().unwrap(),
+            _ => unreachable!(),
         }
     }
 
