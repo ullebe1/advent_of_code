b637e4c125103ef35c9e855234759aa6faa51f62
Ulrik Boll Djurtoft <Ullebe1@gmail.com>
2022-12-03 15:51:45 +0100

    Add 2022 Day 3 scaffolding

---
+++ 2022/src/day3.rs
@@ -0,0 +1,23 @@
+use std::collections::HashSet;
+
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[derive(Debug, Clone)]
+pub struct Input {
+    rounds: Vec<HashSet<char>>,
+}
+
+#[aoc_generator(day3)]
+pub fn input_generator(input: &str) -> Input {
+    unimplemented!()
+}
+
+#[aoc(day3, part1)]
+pub fn part1(input: &Input) -> usize {
+    unimplemented!()
+}
+
+#[aoc(day3, part2)]
+pub fn part2(input: &Input) -> usize {
+    unimplemented!()
+}
