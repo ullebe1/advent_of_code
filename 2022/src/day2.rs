06b167ad8a05aa148de11f1fe9afcd87adf58228
Ulrik Boll Djurtoft <Ullebe1@gmail.com>
2022-12-03 12:46:17 +0100

    Add 2022 Day 2 scaffolding

---
+++ 2022/src/day2.rs
@@ -0,0 +1,21 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[derive(Debug)]
+pub struct Input {
+    elves: Vec<Vec<usize>>,
+}
+
+#[aoc_generator(day2)]
+pub fn input_generator(input: &str) -> Input {
+    unimplemented!();
+}
+
+#[aoc(day2, part1)]
+pub fn part1(input: &Input) -> usize {
+    unimplemented!();
+}
+
+#[aoc(day2, part2)]
+pub fn part2(input: &Input) -> usize {
+    unimplemented!();
+}
--- 2022/src/lib.rs
+++ 2022/src/lib.rs
@@ -4,5 +4,6 @@
 use aoc_runner_derive::aoc_lib;
 
 mod day1;
+mod day2;
 
 aoc_lib! { year = 2022 }
