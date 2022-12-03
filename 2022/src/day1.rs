e8b0e2344df80c08c339351248800646df592364
Ulrik Boll Djurtoft <Ullebe1@gmail.com>
2022-12-03 12:21:32 +0100

    Set up initial scaffolding for 2022 and day 1

---
+++ 2022/src/day1.rs
@@ -0,0 +1,21 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[derive(Debug)]
+pub struct Input {
+    numbers: Vec<Vec<usize>>,
+}
+
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Input {
+    unimplemented!();
+}
+
+#[aoc(day1, part1)]
+pub fn part1(input: &Input) -> usize {
+    unimplemented!()
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &Input) -> usize {
+    unimplemented!()
+}
---
+++ 2022/src/lib.rs
@@ -0,0 +1,8 @@
+#![feature(iter_advance_by)]
+#![feature(map_try_insert)]
+
+use aoc_runner_derive::aoc_lib;
+
+mod day1;
+
+aoc_lib! { year = 2022 }
