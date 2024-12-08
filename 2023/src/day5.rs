4ae19d10c00839d6873ce390daceaf6ba7d60f65
Ulrik Boll Djurtoft <ulrik.boll.djurtoft@lego.com>
2024-12-08 13:50:54 +0100

    More leftover 2023 changes

--- 2023/src/day5.rs
+++ 2023/src/day5.rs
@@ -1,104 +1,115 @@
+use std::ops::Range;
+use std::slice::Iter;
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[derive(Debug)]
 pub struct Input {
-    calibrations: Vec<String>,
+    seeds: Vec<usize>,
+    seed_to_soil: Vec<(Range<usize>, Range<usize>)>,
+    soil_to_fertilizer: Vec<(Range<usize>, Range<usize>)>,
+    fertilizer_to_water: Vec<(Range<usize>, Range<usize>)>,
+    water_to_light: Vec<(Range<usize>, Range<usize>)>,
+    light_to_temperature: Vec<(Range<usize>, Range<usize>)>,
+    temperature_to_humidity: Vec<(Range<usize>, Range<usize>)>,
+    humidity_to_location: Vec<(Range<usize>, Range<usize>)>,
 }
 
-#[aoc_generator(day1)]
+#[aoc_generator(day5)]
 pub fn input_generator(input: &str) -> Input {
-    let lines = input.lines();
-
-    let calibrations = lines.map(|line| line.to_string()).collect::<Vec<String>>();
-
-    Input { calibrations }
+    let binding = input.lines().collect::<Vec<&str>>();
+    let mut lines = binding.iter();
+    
+    // Get seeds
+    let (_, seeds) = lines.next().unwrap().split_once(": ").unwrap();
+    let seeds = seeds.split(" ").map(|s| s.parse::<usize>().unwrap()).collect::<Vec<usize>>();
+    
+    // Get seed-to-soil map:
+    lines.next();
+    lines.next();
+    let (seed_to_soil, mut lines) = get_ranges(&mut lines);
+    
+    // Get soil-to-fertilizer map:
+    lines.next();
+    lines.next();
+    let (soil_to_fertilizer, mut lines) = get_ranges(&mut lines);
+    
+    // Get fertilizer-to-water map:
+    lines.next();
+    lines.next();
+    let (fertilizer_to_water, mut lines) = get_ranges(&mut lines);
+    
+    // Get water-to-light map:
+    lines.next();
+    lines.next();
+    let (water_to_light, mut lines) = get_ranges(&mut lines);
+    
+    // Get light-to-temperature map:
+    lines.next();
+    lines.next();
+    let (light_to_temperature, mut lines) = get_ranges(&mut lines);
+    
+    // Get temperature-to-humidity map:
+    lines.next();
+    lines.next();
+    let (temperature_to_humidity, mut lines) = get_ranges(&mut lines);
+    
+    // Get humidity-to-location map:
+    lines.next();
+    lines.next();
+    let (humidity_to_location, _) = get_ranges(&mut lines);
+    
+    Input {
+        seeds,
+        seed_to_soil,
+        soil_to_fertilizer,
+        fertilizer_to_water,
+        water_to_light,
+        light_to_temperature,
+        temperature_to_humidity,
+        humidity_to_location,
+    }
 }
 
-#[aoc(day1, part1)]
+fn get_ranges<'a>(lines: &'a mut Iter<'_, &str>) -> (Vec<(Range<usize>, Range<usize>)>, Iter<'a, &'a str>) {
+    let mut ranges = Vec::new();
+    while let Some(line) = lines.next() {
+        if line.is_empty() {
+            break;
+        }
+        let (destination, source_and_length) = line.split_once(" ").unwrap();
+        let (source, length) = source_and_length.split_once(" ").unwrap();
+        let destination = destination.parse::<usize>().unwrap();
+        let source = source.parse::<usize>().unwrap();
+        let length = length.parse::<usize>().unwrap();
+        ranges.push((Range { start: destination, end: destination + length }, Range { start: source, end: source + length }));
+    }
+
+    (ranges, lines.to_owned())
+}
+
+pub fn get_corresponding(number: usize, range1: Range<usize>, range2: Range<usize>) -> usize {
+    let index = range1.clone().position(|n| n == number).unwrap();
+    range2.clone().nth(index).unwrap()
+}
+
+#[aoc(day5, part1)]
 pub fn part1(input: &Input) -> usize {
-    let mut digits = Vec::new();
-
-    for calibration in input.calibrations.clone() {
-        let mut first = None;
-        let mut second = None;
-        println!("{}", calibration);
-        for c in calibration.chars() {
-            println!("  {}", c);
-            if c.is_digit(10) {
-                if first.is_none() {
-                    first = Some(c);
-                    println!("    first: {}", first.unwrap());
-                } else {
-                    second = Some(c);
-                    println!("    second: {}", second.unwrap());
-                }
-            }
-        }
-        if first.is_some() && second.is_some() {
-            digits.push(first.unwrap().to_string() + &second.unwrap().to_string());
-        } else if first.is_some() {
-            digits.push(first.unwrap().to_string() + &first.unwrap().to_string());
+    let mut lowest = 0;
+    
+    for seed in input.seeds.clone() {
+        println!("Seed: {}", seed);
+        let mut index = seed;
+        index = get_corresponding(index, input.seed_to_soil[0].0.clone(), input.seed_to_soil[0].1.clone());
+        index = get_corresponding(index, input.soil_to_fertilizer[0].0.clone(), input.soil_to_fertilizer[0].1.clone());
+        index = get_corresponding(index, input.fertilizer_to_water[0].0.clone(), input.fertilizer_to_water[0].1.clone());
+        index = get_corresponding(index, input.water_to_light[0].0.clone(), input.water_to_light[0].1.clone());
+        index = get_corresponding(index, input.light_to_temperature[0].0.clone(), input.light_to_temperature[0].1.clone());
+        index = get_corresponding(index, input.temperature_to_humidity[0].0.clone(), input.temperature_to_humidity[0].1.clone());
+        index = get_corresponding(index, input.humidity_to_location[0].0.clone(), input.humidity_to_location[0].1.clone());
+        if lowest == 0 || index < lowest {
+            lowest = index;
         }
     }
-
-    digits.iter().map(|s| s.parse::<usize>().unwrap()).sum()
-}
-
-#[aoc(day1, part2)]
-pub fn part2(input: &Input) -> usize {
-    let mut digits = Vec::new();
-
-    for mut calibration in input.calibrations.clone() {
-        let mut first = None;
-        let mut second = None;
-        println!("{}", calibration);
-        loop {
-            if calibration.is_empty() {
-                break;
-            }
-            let digit = match calibration.chars().collect::<Vec<char>>().as_slice() {
-                ['1', ..] => Some('1'),
-                ['2', ..] => Some('2'),
-                ['3', ..] => Some('3'),
-                ['4', ..] => Some('4'),
-                ['5', ..] => Some('5'),
-                ['6', ..] => Some('6'),
-                ['7', ..] => Some('7'),
-                ['8', ..] => Some('8'),
-                ['9', ..] => Some('9'),
-                ['0', ..] => Some('0'),
-                ['o', 'n', 'e', ..] => Some('1'),
-                ['t', 'w', 'o', ..] => Some('2'),
-                ['t', 'h', 'r', 'e', 'e', ..] => Some('3'),
-                ['f', 'o', 'u', 'r', ..] => Some('4'),
-                ['f', 'i', 'v', 'e', ..] => Some('5'),
-                ['s', 'i', 'x', ..] => Some('6'),
-                ['s', 'e', 'v', 'e', 'n', ..] => Some('7'),
-                ['e', 'i', 'g', 'h', 't', ..] => Some('8'),
-                ['n', 'i', 'n', 'e', ..] => Some('9'),
-                ['z', 'e', 'r', 'o', ..] => Some('0'),
-                _ => None,
-            };
-
-            calibration = calibration.chars().skip(1).collect::<String>();
-
-            if digit.is_some() {
-                if first.is_none() {
-                    first = digit;
-                    println!("    first: {}", first.unwrap());
-                } else {
-                    second = digit;
-                    println!("    second: {}", second.unwrap());
-                }
-            }
-        }
-
-        if first.is_some() && second.is_some() {
-            digits.push(first.unwrap().to_string() + &second.unwrap().to_string());
-        } else if first.is_some() {
-            digits.push(first.unwrap().to_string() + &first.unwrap().to_string());
-        }
-    }
-
-    digits.iter().map(|s| s.parse::<usize>().unwrap()).sum()
+    
+    lowest
 }
